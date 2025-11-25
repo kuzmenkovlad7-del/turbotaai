d7a8869a9a8988977cd76991a30bd02539410a12
kuzmenkovlad7-del <kuzmenkovlad7@gmail.com>
2025-11-25 21:13:58 +0200

    Updated logo.tsx

--- components/logo.tsx
+++ components/logo.tsx
@@ -1,19 +1,28 @@
-// components/logo.tsx
 "use client"
 
 import Image from "next/image"
 import { APP_NAME } from "@/lib/app-config"
 
-export default function Logo() {
+type LogoProps = {
+  size?: number
+}
+
+export default function Logo({ size = 40 }: LogoProps) {
   return (
-    <div className="relative h-9 w-9 sm:h-10 sm:w-10">
-      <Image
-        src="/logo-turbotaai.svg"
-        alt={APP_NAME}
-        fill
-        priority
-        sizes="40px"
-      />
+    <div className="relative flex items-center">
+      <div
+        className="relative overflow-hidden rounded-full"
+        style={{ width: size, height: size }}
+      >
+        <Image
+          src="/icon.png"            // круг с руками
+          alt={`${APP_NAME} logo`}
+          fill
+          sizes={`${size}px`}
+          className="object-contain"
+          priority
+        />
+      </div>
     </div>
   )
 }
