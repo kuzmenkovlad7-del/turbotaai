640351fc55623c49fa535cb22e61113094210069
kuzmenkovlad7-del <kuzmenkovlad7@gmail.com>
2025-12-04 13:29:57 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,6 @@
-// next.config.mjs
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // appDir включён по умолчанию в Next.js 13+,
-  // поэтому здесь его указывать не нужно.
   images: {
     remotePatterns: [
       {
